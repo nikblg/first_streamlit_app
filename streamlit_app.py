1851b8f8375cf33b44759de009a26b6a117b2eb5
Nik Sebastijan Blagovič <109960706+nikblg@users.noreply.github.com>
2022-07-28 09:18:01 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit
 
 streamlit.title("My Parents New Healthy Diner")
-streamlit.header("Breakfast Menu")
-streamlit.text("1111lbablalblalbl")
-streamlit.text("2222lbablalblalbl")
-streamlit.text("3333lbablalblalbl")
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg'
