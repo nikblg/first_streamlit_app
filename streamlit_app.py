d6096998027b3cc31cee2ac40f2e68ecec9acc9e
Nik Sebastijan Blagovič <109960706+nikblg@users.noreply.github.com>
2022-07-28 09:16:42 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,7 @@
 import streamlit
 
 streamlit.title("My Parents New Healthy Diner")
+streamlit.header("Breakfast Menu")
+streamlit.text("1111lbablalblalbl")
+streamlit.text("2222lbablalblalbl")
+streamlit.text("3333lbablalblalbl")
