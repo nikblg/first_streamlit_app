a75abf4b3a0dbb8ad96ff607493fc284204925c8
Nik Sebastijan Blagovič <109960706+nikblg@users.noreply.github.com>
2022-07-29 13:04:37 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -43,9 +43,6 @@ streamlit.write('The user entered ', fruit_choice)
 
 # write your own comment - what does this do?
 
-
-streamlit.stop()
-
 def get_fruit_load_list():
   with my_cnx.cursor() as my_cur:
     my_cur.execute("select * from fruit_load_list")
